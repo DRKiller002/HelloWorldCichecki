f9e97038424b0f958279a732ead3ef83eaf0432c
Jakub Cichecki <jcichec1@stu.vistula.edu.pl>
2022-10-06 18:08:58 +0200

    second commit

--- src/pl/vistula/helloworldcichecki/HelloWorldCichecki.java
+++ src/pl/vistula/helloworldcichecki/HelloWorldCichecki.java
@@ -3,5 +3,6 @@ package pl.vistula.helloworldcichecki;
 public class HelloWorldCichecki {
     public static void main(String[] args) {
         System.out.println("Hello Vistula Cichecki");
+        System.out.println("second commit");
     }
 }
