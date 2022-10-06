c2360c8b5b8ec3bd467eeee9be022d5988a9706a
Jakub Cichecki <jcichec1@stu.vistula.edu.pl>
2022-10-06 17:59:37 +0200

    Initial commit

---
+++ src/pl/vistula/helloworldcichecki/HelloWorldCichecki.java
@@ -0,0 +1,7 @@
+package pl.vistula.helloworldcichecki;
+
+public class HelloWorldCichecki {
+    public static void main(String[] args) {
+        System.out.println("Hello Vistula Cichecki");
+    }
+}
